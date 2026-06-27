8598e2a31d0d9a2897da6cb8a7aebcacb383399e
guialmm <guilherme.almeida@ges.inatel.br>
2026-06-27 18:29:52 -0300

    fix: add .onrender.com to ALLOWED_HOSTS

--- core/settings.py
+++ core/settings.py
@@ -8,7 +8,7 @@ environ.Env.read_env(BASE_DIR / ".env")
 
 SECRET_KEY = env("SECRET_KEY", default="django-insecure-change-me-in-production")
 DEBUG = env("DEBUG")
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["localhost", "127.0.0.1", ".railway.app", ".up.railway.app"])
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["localhost", "127.0.0.1", ".railway.app", ".up.railway.app", ".onrender.com"])
 
 INSTALLED_APPS = [
     "django.contrib.admin",
